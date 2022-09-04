509effabd763df2740e3198cfb23914dac38f3a4
s22985-pj <s22985@pjwstk.edu.pl>
2022-09-04 14:21:32 +0200

    1st

---
+++ s22985/src/main/java/com/example/s22985/S22985Application.java
@@ -0,0 +1,13 @@
+package com.example.s22985;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S22985Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S22985Application.class, args);
+	}
+
+}
---
+++ s22985/src/test/java/com/example/s22985/S22985ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s22985;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S22985ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
